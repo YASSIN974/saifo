ee3ca99b1e5cb33c0b3f6b444f0b2f1ea70223c7
YASSIN974 <44032576+YASSIN974@users.noreply.github.com>
2018-10-31 13:00:57 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const fs = require("fs");
-const ownerid = '439187325503930369'
+const ownerid = '332713449215754242'
 client.colors = {}
 const prefix = "*"
 //let rainbow = 0;
